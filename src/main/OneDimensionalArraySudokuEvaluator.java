ca6b016b3dd211aa6978003a5b77daa1e952e747
Aboud Chamoun <aboud.chamoun@yahoo.com>
2021-02-28 12:09:21 +0100

    generating methods to evaluate the one dimensional array sudoku

--- src/main/OneDimensionalArraySudokuEvaluator.java
+++ src/main/OneDimensionalArraySudokuEvaluator.java
@@ -1,8 +1,43 @@
 package main;
 
+
+/**
+ * 
+ * @author aboud
+ *	
+ * methods to evaluate a sudoku represented in a one dimensional array.
+ * the methods are meant to evaluate a sudoku with 9x9 grid and 3x3 blocks. 
+ *	
+ */
 public class OneDimensionalArraySudokuEvaluator {
-	
+
 	public boolean evaluateSudoku(int[] sudokuArray) {
+		if (evaluateRows(sudokuArray) && evaluateColoumns(sudokuArray) && evaluateBlocks(sudokuArray))
+			return true;
+		return false;
+	}
+
+	private boolean evaluateRows(int[] sudokuArrays) {
+		return false;
+	}
+
+	private boolean evaluateColoumns(int[] sudokuArrays) {
+		return false;
+	}
+
+	private boolean evaluateBlocks(int[] sudokuArrays) {
+		return false;
+	}
+
+	private boolean evaluateRow(int[] row) {
+		return false;
+	}
+
+	private boolean evaluateColoumn(int[] coloumn) {
+		return false;
+	}
+
+	private boolean evaluateBlock(int[] block) {
 		return false;
 	}
 }
