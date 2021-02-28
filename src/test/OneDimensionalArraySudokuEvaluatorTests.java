35f9414129713c5dc53f02f7009c0df4e6de6725
Aboud Chamoun <aboud.chamoun@yahoo.com>
2021-02-28 11:43:51 +0100

    generating test cases for the one dimensional array sudoku

--- src/test/OneDimensionalArraySudokuEvaluatorTests.java
+++ src/test/OneDimensionalArraySudokuEvaluatorTests.java
@@ -1,5 +1,6 @@
 package test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,19 @@ import main.OneDimensionalArraySudokuEvaluator;
  */
 public class OneDimensionalArraySudokuEvaluatorTests {
 
-	int[] sudoku = { 5, 3, 4, 6, 7, 8, 9, 1, 2, 6, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9, 7, 6, 1,
+	int[] sudokuTrue = { 5, 3, 4, 6, 7, 8, 9, 1, 2, 6, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9, 7, 6, 1,
+			4, 2, 3, 4, 2, 6, 8, 5, 3, 7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8, 7, 4, 1, 9,
+			6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9 };
+	
+	int[] sudokuFalseBlock = { 5, 3, 4, 6, 7, 8, 5, 1, 2, 6, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9, 7, 6, 1,
+			4, 2, 3, 4, 2, 6, 8, 5, 3, 7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8, 7, 4, 1, 9,
+			6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9 };
+	
+	int[] sudokuFalseColumn = { 5, 3, 4, 6, 7, 8, 9, 1, 2, 5, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9, 7, 6, 1,
+			4, 2, 3, 4, 2, 6, 8, 5, 3, 7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8, 7, 4, 1, 9,
+			6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9 };
+	
+	int[] sudokuFalseLine = { 5, 3, 4, 6, 7, 8, 9, 1, 2, 5, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9, 7, 6, 1,
 			4, 2, 3, 4, 2, 6, 8, 5, 3, 7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8, 7, 4, 1, 9,
 			6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9 };
 
@@ -23,7 +36,10 @@ public class OneDimensionalArraySudokuEvaluatorTests {
 
 	@Test
 	void evaluateSudokuTest() {
-		assertTrue(validator.evaluateSudoku(sudoku));
+		assertTrue(validator.evaluateSudoku(sudokuTrue));
+		assertFalse(validator.evaluateSudoku(sudokuFalseBlock));
+		assertFalse(validator.evaluateSudoku(sudokuFalseColumn));
+		assertFalse(validator.evaluateSudoku(sudokuFalseLine));
 	}
 
 }
