d87f4296858beec259f83699e5685bfef21c599d
Aboud Chamoun <aboud.chamoun@yahoo.com>
2021-03-08 15:30:59 +0100

    implementing a method to find out double values in an array. implementing tests for this method

--- src/main/OneDimensionalArraySudokuEvaluator.java
+++ src/main/OneDimensionalArraySudokuEvaluator.java
@@ -74,7 +74,7 @@ public class OneDimensionalArraySudokuEvaluator {
 	private boolean evaluateRows(int[][] rows) {
 
 		for (int i = 0; i < rows.length; i++) {
-			if (!evaluateArray(rows[i]))
+			if (!findEqualValues(rows[i]))
 				return false;
 		}
 		return true;
@@ -83,18 +83,41 @@ public class OneDimensionalArraySudokuEvaluator {
 	private boolean evaluateColumns(int[][] columns) {
 
 		for (int i = 0; i < columns.length; i++) {
-			if (!evaluateArray(columns[i]))
+			if (!findEqualValues(columns[i]))
 				return false;
 		}
 		return true;
 	}
 
 	private boolean evaluateBlocks(int[][] blocks) {
-		return false;
+
+		for (int i = 0; i < blocks.length; i++) {
+			if (!findEqualValues(blocks[i]))
+				return false;
+		}
+
+		return true;
 	}
 
-	private boolean evaluateArray(int[] is) {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 * this method will be recursively called 
+	 * to find out equal values in the array
+	 * 
+	 * @param arr
+	 * @return
+	 */
+	public boolean findEqualValues(int[] arr) {
+
+		if (arr.length == 1)
+			return true;
+		int[] restArray = new int[arr.length - 1];
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[0] == arr[i]) {
+				return false;
+			} else {
+				restArray[i - 1] = arr[i];
+			}
+		}
+		return findEqualValues(restArray);
 	}
 }
--- src/test/OneDimensionalArraySudokuEvaluatorTests.java
+++ src/test/OneDimensionalArraySudokuEvaluatorTests.java
@@ -9,28 +9,28 @@ import main.OneDimensionalArraySudokuEvaluator;
 
 /**
  * 
- * @author aboud
- * Tests for the one dimensional array evaluator.
- * the array is a linear representation of the sudoku in https://en.wikipedia.org/wiki/Sudoku
+ * @author aboud Tests for the one dimensional array evaluator. the array is a
+ *         linear representation of the sudoku in
+ *         https://en.wikipedia.org/wiki/Sudoku
  * 
  */
 public class OneDimensionalArraySudokuEvaluatorTests {
 
-	int[] sudokuTrue = { 5, 3, 4, 6, 7, 8, 9, 1, 2, 6, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9, 7, 6, 1,
-			4, 2, 3, 4, 2, 6, 8, 5, 3, 7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8, 7, 4, 1, 9,
-			6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9 };
-	
-	int[] sudokuFalseBlock = { 5, 3, 4, 6, 7, 8, 5, 1, 2, 6, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9, 7, 6, 1,
-			4, 2, 3, 4, 2, 6, 8, 5, 3, 7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8, 7, 4, 1, 9,
-			6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9 };
-	
-	int[] sudokuFalseColumn = { 5, 3, 4, 6, 7, 8, 9, 1, 2, 5, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9, 7, 6, 1,
-			4, 2, 3, 4, 2, 6, 8, 5, 3, 7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8, 7, 4, 1, 9,
-			6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9 };
-	
-	int[] sudokuFalseLine = { 5, 3, 4, 6, 7, 8, 9, 1, 2, 5, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9, 7, 6, 1,
-			4, 2, 3, 4, 2, 6, 8, 5, 3, 7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8, 7, 4, 1, 9,
-			6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9 };
+	int[] sudokuTrue = { 5, 3, 4, 6, 7, 8, 9, 1, 2, 6, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9, 7, 6,
+			1, 4, 2, 3, 4, 2, 6, 8, 5, 3, 7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8, 7, 4, 1,
+			9, 6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9 };
+
+	int[] sudokuFalseBlock = { 5, 3, 4, 6, 7, 8, 5, 1, 2, 6, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9,
+			7, 6, 1, 4, 2, 3, 4, 2, 6, 8, 5, 3, 7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8, 7,
+			4, 1, 9, 6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9 };
+
+	int[] sudokuFalseColumn = { 5, 3, 4, 6, 7, 8, 9, 1, 2, 5, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5,
+			9, 7, 6, 1, 4, 2, 3, 4, 2, 6, 8, 5, 3, 7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8,
+			7, 4, 1, 9, 6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9 };
+
+	int[] sudokuFalseLine = { 5, 3, 4, 6, 7, 8, 9, 1, 2, 5, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9,
+			7, 6, 1, 4, 2, 3, 4, 2, 6, 8, 5, 3, 7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8, 7,
+			4, 1, 9, 6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9 };
 
 	OneDimensionalArraySudokuEvaluator validator = new OneDimensionalArraySudokuEvaluator();
 
@@ -40,6 +40,17 @@ public class OneDimensionalArraySudokuEvaluatorTests {
 		assertFalse(validator.evaluateSudoku(sudokuFalseBlock));
 		assertFalse(validator.evaluateSudoku(sudokuFalseColumn));
 		assertFalse(validator.evaluateSudoku(sudokuFalseLine));
+
+	}
+
+	@Test
+	void findEqualValuesTest() {
+		assertTrue(validator.findEqualValues(new int[] { 1, 3, 5, 6, 7, 8, 9, 0 }));
+		assertTrue(validator.findEqualValues(new int[] { 8 }));
+		assertFalse(validator.findEqualValues(new int[] { 1, 3, 5, 6, 5, 8, 9, 0 }));
+		assertFalse(validator.findEqualValues(new int[] { 1, 3, 5, 6, 5, 8, 9, 1 }));
+		assertFalse(validator.findEqualValues(new int[] { 1, 3, 5, 6, 5, 8, 9, 9 }));
+
 	}
 
 }
